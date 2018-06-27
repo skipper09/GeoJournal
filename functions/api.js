1cf02398a89452fb785727dd24e7b8dbc8b1d9d5
Sarah Payne <sarah@Sarahs-MacBook-Pro.local>
2018-06-27 09:28:49 -0500

    set up google cloud functions

---
+++ functions/api.js
@@ -0,0 +1,15 @@
+//const app = express();
+
+// Automatically allow cross-origin requests
+//app.use(cors({ origin: true }));
+
+// Add middleware if you want.
+//app.use(myMiddleware);
+
+// build multiple CRUD interfaces:
+//app.get('/news', (req, res) => {//...});
+//app.post('/', (req, res) => {//...});
+//app.put('/:id', (req, res) => {//...});
+//app.delete('/:id', (req, res) => {//...});
+// Expose Express API as a single Cloud Function:
+// exports.myAwesomeFunction = functions.https.onRequest(app);
